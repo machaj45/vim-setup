b9d2e70d52d2c84aaa57a4be912d3811a83a2d6a
machaj45 <machaj45@fel.cvut.cz>
2021-03-20 10:45:13 +0100

    install now install vim

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 git submodule update --init --recursive
+sudo apt-get --yes  install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
+cd ./vim/src
+make distclean
+./configure --with-features=huge
+make
+sudo make install
+cd ..

12962241634f22ecb7e81da2f8d18871f5cf4791
machaj45 <machaj45@fel.cvut.cz>
2021-03-20 12:12:05 +0100

    added conditional installation of vim

--- install.sh
+++ install.sh
@@ -1,9 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+
+# Updating repository from github
+git pull
+
+# Updating all submodules form github
 git submodule update --init --recursive
-sudo apt-get --yes  install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
-cd ./vim/src
-make distclean
-./configure --with-features=huge
-make
-sudo make install
-cd ..
+
+# Geting all necessary libs for vim build
+sudo apt-get --yes  install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
+
+# Building Vim it selfe
+isVIM=(`vim --version | grep "Compiled by machy*" | wc -l`) 
+
+if [ $isVIM -eq 0 ]
+	then
+	cd ./vim/src
+	make distclean
+	./configure --with-features=huge
+	make
+	sudo make install
+	cd ../..
+fi
+
+# Cleaning up
+sudo apt-get --yes autoremove
+
+# Linking .vim and .vimrc to their position
+mkdir ~/.vim 2> /dev/null || rm -rf ~/.vim/*
+ln -sr dotvim/* ~/.vim
+ln -sr vimrc ~/.vimrc 2> /dev/null
--- vimrc
+++ vimrc
@@ -1,2 +1,8 @@
 set number
 set relativenumber
+set hlsearch
+syntax on
+command WQ wq
+command Wq wq
+command W w
+command Q q
